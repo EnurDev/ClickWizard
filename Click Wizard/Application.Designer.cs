b213f35d6b75785c5dc0c63a6266b32b730f742b
Enur <enurbehchet@gmail.com>
2024-01-27 18:25:58 +0200

    Initial commit

---
+++ Click Wizard/Application.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Click_Wizard
+{
+    partial class Application
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Application));
+            this.appTitle = new System.Windows.Forms.Label();
+            this.millisecondsLabel = new System.Windows.Forms.Label();
+            this.secondsLabel = new System.Windows.Forms.Label();
+            this.minutesLabel = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.minutesText = new System.Windows.Forms.TextBox();
+            this.secondsText = new System.Windows.Forms.TextBox();
+            this.millisecondsText = new System.Windows.Forms.TextBox();
+            this.leftRadio = new System.Windows.Forms.RadioButton();
+            this.middleRadio = new System.Windows.Forms.RadioButton();
+            this.rightRadio = new System.Windows.Forms.RadioButton();
+            this.clickerstartButton = new System.Windows.Forms.Button();
+            this.clickerstopButton = new System.Windows.Forms.Button();
+            this.clickerStatus = new System.Windows.Forms.Label();
+            this.cpsCounter = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // appTitle
+            // 
+            this.appTitle.Location = new System.Drawing.Point(34, 34);
+            this.appTitle.Name = "appTitle";
+            this.appTitle.Size = new System.Drawing.Size(385, 33);
+            this.appTitle.TabIndex = 0;
+            this.appTitle.Text = "[APPLICATION TITLE]";
+            // 
+            // millisecondsLabel
+            // 
+            this.millisecondsLabel.Location = new System.Drawing.Point(6, 19);
+            this.millisecondsLabel.Name = "millisecondsLabel";
+            this.millisecondsLabel.Size = new System.Drawing.Size(100, 23);
+            this.millisecondsLabel.TabIndex = 4;
+            this.millisecondsLabel.Text = "[Miliseconds Label]";
+            // 
+            // secondsLabel
+            // 
+            this.secondsLabel.Location = new System.Drawing.Point(137, 19);
+            this.secondsLabel.Name = "secondsLabel";
+            this.secondsLabel.Size = new System.Drawing.Size(100, 23);
+            this.secondsLabel.TabIndex = 5;
+            this.secondsLabel.Text = "[Seconds Label]";
+            // 
+            // minutesLabel
+            // 
+            this.minutesLabel.Location = new System.Drawing.Point(265, 19);
+            this.minutesLabel.Name = "minutesLabel";
+            this.minutesLabel.Size = new System.Drawing.Size(100, 23);
+            this.minutesLabel.TabIndex = 6;
+            this.minutesLabel.Text = "[Minutes Label]";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.minutesText);
+            this.groupBox1.Controls.Add(this.secondsText);
+            this.groupBox1.Controls.Add(this.millisecondsText);
+            this.groupBox1.Controls.Add(this.millisecondsLabel);
+            this.groupBox1.Controls.Add(this.minutesLabel);
+            this.groupBox1.Controls.Add(this.secondsLabel);
+            this.groupBox1.Location = new System.Drawing.Point(34, 89);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(385, 84);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[Time Unit GroupBox]";
+            // 
+            // minutesText
+            // 
+            this.minutesText.Location = new System.Drawing.Point(265, 45);
+            this.minutesText.Name = "minutesText";
+            this.minutesText.Size = new System.Drawing.Size(100, 20);
+            this.minutesText.TabIndex = 9;
+            // 
+            // secondsText
+            // 
+            this.secondsText.Location = new System.Drawing.Point(137, 45);
+            this.secondsText.Name = "secondsText";
+            this.secondsText.Size = new System.Drawing.Size(100, 20);
+            this.secondsText.TabIndex = 8;
+            // 
+            // millisecondsText
+            // 
+            this.millisecondsText.Location = new System.Drawing.Point(6, 45);
+            this.millisecondsText.Name = "millisecondsText";
+            this.millisecondsText.Size = new System.Drawing.Size(100, 20);
+            this.millisecondsText.TabIndex = 7;
+            // 
+            // leftRadio
+            // 
+            this.leftRadio.Location = new System.Drawing.Point(40, 179);
+            this.leftRadio.Name = "leftRadio";
+            this.leftRadio.Size = new System.Drawing.Size(100, 24);
+            this.leftRadio.TabIndex = 8;
+            this.leftRadio.TabStop = true;
+            this.leftRadio.Text = "[LeftRadio]";
+            this.leftRadio.UseVisualStyleBackColor = true;
+            // 
+            // middleRadio
+            // 
+            this.middleRadio.Location = new System.Drawing.Point(171, 179);
+            this.middleRadio.Name = "middleRadio";
+            this.middleRadio.Size = new System.Drawing.Size(100, 24);
+            this.middleRadio.TabIndex = 9;
+            this.middleRadio.TabStop = true;
+            this.middleRadio.Text = "[MiddleRadio]";
+            this.middleRadio.UseVisualStyleBackColor = true;
+            // 
+            // rightRadio
+            // 
+            this.rightRadio.Location = new System.Drawing.Point(299, 179);
+            this.rightRadio.Name = "rightRadio";
+            this.rightRadio.Size = new System.Drawing.Size(100, 24);
+            this.rightRadio.TabIndex = 10;
+            this.rightRadio.TabStop = true;
+            this.rightRadio.Text = "[RightRadio]";
+            this.rightRadio.UseVisualStyleBackColor = true;
+            // 
+            // clickerstartButton
+            // 
+            this.clickerstartButton.Location = new System.Drawing.Point(34, 223);
+            this.clickerstartButton.Name = "clickerstartButton";
+            this.clickerstartButton.Size = new System.Drawing.Size(185, 37);
+            this.clickerstartButton.TabIndex = 11;
+            this.clickerstartButton.Text = "[START BUTTON]";
+            this.clickerstartButton.UseVisualStyleBackColor = true;
+            this.clickerstartButton.Click += new System.EventHandler(this.clickerstartButton_Click);
+            // 
+            // clickerstopButton
+            // 
+            this.clickerstopButton.Location = new System.Drawing.Point(234, 223);
+            this.clickerstopButton.Name = "clickerstopButton";
+            this.clickerstopButton.Size = new System.Drawing.Size(185, 37);
+            this.clickerstopButton.TabIndex = 12;
+            this.clickerstopButton.Text = "[STOP BUTTON]";
+            this.clickerstopButton.UseVisualStyleBackColor = true;
+            this.clickerstopButton.Click += new System.EventHandler(this.clickerstopButton_Click);
+            // 
+            // clickerStatus
+            // 
+            this.clickerStatus.Location = new System.Drawing.Point(319, 63);
+            this.clickerStatus.Name = "clickerStatus";
+            this.clickerStatus.Size = new System.Drawing.Size(100, 23);
+            this.clickerStatus.TabIndex = 13;
+            this.clickerStatus.Text = "[STATUS]";
+            // 
+            // cpsCounter
+            // 
+            this.cpsCounter.Location = new System.Drawing.Point(34, 63);
+            this.cpsCounter.Name = "cpsCounter";
+            this.cpsCounter.Size = new System.Drawing.Size(100, 23);
+            this.cpsCounter.TabIndex = 14;
+            this.cpsCounter.Text = "[CPS]";
+            // 
+            // Application
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 295);
+            this.Controls.Add(this.cpsCounter);
+            this.Controls.Add(this.clickerStatus);
+            this.Controls.Add(this.clickerstopButton);
+            this.Controls.Add(this.clickerstartButton);
+            this.Controls.Add(this.rightRadio);
+            this.Controls.Add(this.middleRadio);
+            this.Controls.Add(this.leftRadio);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.appTitle);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Application";
+            this.Text = "[Application]";
+            this.Load += new System.EventHandler(this.Application_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Label clickerStatus;
+        private System.Windows.Forms.Label cpsCounter;
+
+        private System.Windows.Forms.TextBox millisecondsText;
+        private System.Windows.Forms.TextBox secondsText;
+        private System.Windows.Forms.TextBox minutesText;
+        private System.Windows.Forms.RadioButton leftRadio;
+        private System.Windows.Forms.RadioButton middleRadio;
+        private System.Windows.Forms.RadioButton rightRadio;
+        private System.Windows.Forms.Button clickerstartButton;
+        private System.Windows.Forms.Button clickerstopButton;
+
+        private System.Windows.Forms.GroupBox groupBox1;
+
+        private System.Windows.Forms.Label appTitle;
+        private System.Windows.Forms.Label secondsLabel;
+        private System.Windows.Forms.Label minutesLabel;
+
+        private System.Windows.Forms.Label millisecondsLabel;
+
+        #endregion
+    }
+}
---
+++ Click Wizard/Application.cs
@@ -0,0 +1,437 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Reflection;
+
+namespace Click_Wizard
+{
+    public partial class Application : Form
+    {
+        private const int WM_HOTKEY = 0x0312;
+        private const int INPUT_MOUSE = 0;
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        private const int MOUSEEVENTF_MIDDLEDOWN = 0x20;
+        private const int MOUSEEVENTF_MIDDLEUP = 0x40;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct INPUT
+        {
+            public uint Type;
+            public MOUSEINPUT MouseInput;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MOUSEINPUT
+        {
+            public int X;
+            public int Y;
+            public uint MouseData;
+            public uint Flags;
+            public uint Time;
+            public IntPtr ExtraInfo;
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint SendInput(uint nInputs, INPUT[] pInputs, int cbSize);
+
+        private System.Threading.Timer timer;
+        private bool autoClickerRunning = false;
+        
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private int hotkeyId = 0;
+
+        
+        public Application()
+        {
+            InitializeComponent();
+            RegisterHotKey();
+        }
+        
+        private void Application_Load(object sender, EventArgs e)
+        {
+            this.Text = "Click Wizard" + " BETA" + " - " + "Version: " + GetAppVersion();
+            this.BackColor = Color.FromArgb(255, 255, 255);
+            this.Size = new Size(465, 325);
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            
+            appTitle.Text = "Click Wizard";
+            appTitle.Font = new Font("Arial", 16, FontStyle.Bold);
+            appTitle.TextAlign = ContentAlignment.MiddleCenter;
+            groupBox1.Text = "Time Unit Configurator";
+            millisecondsText.Text = "0";
+            secondsText.Text = "0";
+            minutesText.Text = "0";
+            
+            clickerstartButton.Text = "Start (F6)";
+            clickerstopButton.Text = "Stop (F6)";
+            leftRadio.Text = "Left Click";
+            leftRadio.Checked = true;
+            middleRadio.Text = "Middle Click";
+            rightRadio.Text = "Right Click";
+            millisecondsLabel.Text = "Miliseconds";
+            secondsLabel.Text = "Seconds";
+            minutesLabel.Text = "Minutes";
+            clickerStatus.Text = "Status: Inactive";
+            clickerStatus.TextAlign = ContentAlignment.MiddleRight;
+            cpsCounter.Text = "CPS: [ 0.00 ]";
+            millisecondsText.TextChanged += TextBox_TextChanged;
+            secondsText.TextChanged += TextBox_TextChanged;
+            minutesText.TextChanged += TextBox_TextChanged;
+            
+            this.KeyPreview = true;
+            this.KeyPress += Application_KeyPress;
+            
+        }
+        
+        private string GetAppVersion()
+        {
+            // Get the entry assembly (main executable) and then get its version
+            Assembly assembly = Assembly.GetEntryAssembly();
+            Version version = assembly.GetName().Version;
+
+            // Format the version as a string without the revision component
+            return $"{version.Major}.{version.Minor}.{version.Build}";
+        }
+        
+        private void RegisterHotKey()
+        {
+            // Register F6 as the global hotkey
+            hotkeyId = 1; // You can choose any unique identifier
+            RegisterHotKey(this.Handle, hotkeyId, 0, (uint)Keys.F6);
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            if (m.Msg == WM_HOTKEY)
+            {
+                // F6 key is pressed
+                if (autoClickerRunning)
+                {
+                    StopAutoClicker();
+                }
+                else
+                {
+                    // Retrieve the values from the text boxes and radio buttons
+                    int milliseconds = int.Parse(millisecondsText.Text);
+                    int seconds = int.Parse(secondsText.Text);
+                    int minutes = int.Parse(minutesText.Text);
+                    bool leftClick = leftRadio.Checked;
+                    bool middleClick = middleRadio.Checked;
+                    bool rightClick = rightRadio.Checked;
+
+                    // Validate the input values
+                    if (milliseconds < 0 || seconds < 0 || minutes < 0)
+                    {
+                        MessageBox.Show("Time values cannot be negative.");
+                        return;
+                    }
+
+                    // Calculate the total time in milliseconds
+                    int totalTime = milliseconds + (seconds * 1000) + (minutes * 60000);
+
+                    // Determine the mouse button to be clicked
+                    string mouseButton = "";
+                    if (leftClick)
+                    {
+                        mouseButton = "left";
+                    }
+                    else if (middleClick)
+                    {
+                        mouseButton = "middle";
+                    }
+                    else if (rightClick)
+                    {
+                        mouseButton = "right";
+                    }
+
+                    // Start the auto clicker
+                    AutoClicker(totalTime, mouseButton);
+                }
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            // Unregister the hotkey when the form is closing
+            UnregisterHotKey(this.Handle, hotkeyId);
+            base.OnFormClosing(e);
+        }
+        
+        private void TextBox_TextChanged(object sender, EventArgs e)
+        {
+            // Calculate CPS based on the values in millisecondsText, secondsText, and minutesText
+            int milliseconds = string.IsNullOrEmpty(millisecondsText.Text) ? 0 : int.Parse(millisecondsText.Text);
+            int seconds = string.IsNullOrEmpty(secondsText.Text) ? 0 : int.Parse(secondsText.Text);
+            int minutes = string.IsNullOrEmpty(minutesText.Text) ? 0 : int.Parse(minutesText.Text);
+
+            double totalTimeInSeconds = milliseconds / 1000.0 + seconds + minutes * 60.0;
+    
+            // Calculate CPS
+            double cps = totalTimeInSeconds > 0 ? 1.0 / totalTimeInSeconds : 0;
+
+            // Display CPS in cpsCounter
+            cpsCounter.Text = $"CPS: [ {cps:F2} ]";
+        }
+        
+        private void clickerstartButton_Click(object sender, EventArgs e)
+        {
+            // Set the default values to 0 if the text boxes are blank
+            millisecondsText.Text = string.IsNullOrEmpty(millisecondsText.Text) ? "0" : millisecondsText.Text;
+            secondsText.Text = string.IsNullOrEmpty(secondsText.Text) ? "0" : secondsText.Text;
+            minutesText.Text = string.IsNullOrEmpty(minutesText.Text) ? "0" : minutesText.Text;
+
+            // Trigger CPS calculation
+            TextBox_TextChanged(null, EventArgs.Empty);
+
+            // Retrieve the values from the text boxes and radio buttons
+            int milliseconds = int.Parse(millisecondsText.Text);
+            int seconds = int.Parse(secondsText.Text);
+            int minutes = int.Parse(minutesText.Text);
+            bool leftClick = leftRadio.Checked;
+            bool middleClick = middleRadio.Checked;
+            bool rightClick = rightRadio.Checked;
+
+            // Validate the input values
+            if (milliseconds < 0 || seconds < 0 || minutes < 0)
+            {
+                MessageBox.Show("Time values cannot be negative.");
+                return;
+            }
+
+            // Calculate the total time in milliseconds
+            int totalTime = milliseconds + (seconds * 1000) + (minutes * 60000);
+
+            // Determine the mouse button to be clicked
+            string mouseButton = "";
+            if (leftClick)
+            {
+                mouseButton = "left";
+            }
+            else if (middleClick)
+            {
+                mouseButton = "middle";
+            }
+            else if (rightClick)
+            {
+                mouseButton = "right";
+            }
+
+            // Start the auto clicker
+            AutoClicker(totalTime, mouseButton);
+        }
+
+
+
+
+        private void AutoClicker(int interval, string mouseButton)
+        {
+            timer = new System.Threading.Timer(_ =>
+            {
+                // Perform the mouse click
+                switch (mouseButton)
+                {
+                    case "left":
+                        SimulateMouseClick(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP);
+                        break;
+                    case "middle":
+                        SimulateMouseClick(MOUSEEVENTF_MIDDLEDOWN | MOUSEEVENTF_MIDDLEUP);
+                        break;
+                    case "right":
+                        SimulateMouseClick(MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP);
+                        break;
+                }
+            }, null, 0, interval);
+
+            // Update the status label
+            autoClickerRunning = true;
+            UpdateStatusLabel();
+
+            // Disable the Start button and Radio buttons
+            clickerstartButton.Enabled = false;
+            leftRadio.Enabled = false;
+            middleRadio.Enabled = false;
+            rightRadio.Enabled = false;
+            millisecondsText.Enabled = false;
+            secondsText.Enabled = false;
+            minutesText.Enabled = false;
+        }
+
+        private void SimulateMouseClick(uint flags)
+        {
+            INPUT[] inputs = new INPUT[1];
+
+            // Press down and then release
+            inputs[0] = new INPUT
+            {
+                Type = INPUT_MOUSE,
+                MouseInput = new MOUSEINPUT
+                {
+                    Flags = flags,
+                    Time = 0
+                }
+            };
+
+            SendInput(1, inputs, Marshal.SizeOf(typeof(INPUT)));
+        }
+
+
+        private void StopAutoClicker()
+        {
+            // Stop the Timer
+            if (timer != null)
+            {
+                timer.Dispose();
+
+                // Update the status label
+                autoClickerRunning = false;
+                UpdateStatusLabel();
+
+                // Enable the Start button and Radio buttons
+                clickerstartButton.Enabled = true;
+                leftRadio.Enabled = true;
+                middleRadio.Enabled = true;
+                rightRadio.Enabled = true;
+                millisecondsText.Enabled = true;
+                secondsText.Enabled = true;
+                minutesText.Enabled = true;
+            }
+        }
+
+        private void UpdateStatusLabel()
+        {
+            // Update the status label based on the auto clicker state
+            clickerStatus.Text = "Status: " + (autoClickerRunning ? "Active" : "Inactive");
+        }
+
+        private void clickerstopButton_Click(object sender, EventArgs e)
+        {
+            StopAutoClicker();
+        }
+
+        private void Application_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Check if the pressed key is F6
+            if (e.KeyChar == (char)Keys.F6)
+            {
+                
+                if (autoClickerRunning)
+                {
+                    StopAutoClicker();
+                }
+                else
+                {
+                    // Retrieve the values from the text boxes and radio buttons
+                    int milliseconds = int.Parse(millisecondsText.Text);
+                    int seconds = int.Parse(secondsText.Text);
+                    int minutes = int.Parse(minutesText.Text);
+                    bool leftClick = leftRadio.Checked;
+                    bool middleClick = middleRadio.Checked;
+                    bool rightClick = rightRadio.Checked;
+
+                    // Validate the input values
+                    if (milliseconds < 0 || seconds < 0 || minutes < 0)
+                    {
+                        MessageBox.Show("Time values cannot be negative.");
+                        return;
+                    }
+
+                    // Calculate the total time in milliseconds
+                    int totalTime = milliseconds + (seconds * 1000) + (minutes * 60000);
+
+                    // Determine the mouse button to be clicked
+                    string mouseButton = "";
+                    if (leftClick)
+                    {
+                        mouseButton = "left";
+                    }
+                    else if (middleClick)
+                    {
+                        mouseButton = "middle";
+                    }
+                    else if (rightClick)
+                    {
+                        mouseButton = "right";
+                    }
+
+                    // Start the auto clicker
+                    AutoClicker(totalTime, mouseButton);
+                }
+            }
+        }
+        
+        private void Apication_KeyDown(object sender, KeyEventArgs e)
+        {
+            // Check if the pressed key is F6
+            if (e.KeyCode == Keys.F6)
+            {
+                if (autoClickerRunning)
+                {
+                    StopAutoClicker();
+                }
+                else
+                {
+                    // Retrieve the values from the text boxes and radio buttons
+                    int milliseconds = int.Parse(millisecondsText.Text);
+                    int seconds = int.Parse(secondsText.Text);
+                    int minutes = int.Parse(minutesText.Text);
+                    bool leftClick = leftRadio.Checked;
+                    bool middleClick = middleRadio.Checked;
+                    bool rightClick = rightRadio.Checked;
+
+                    // Validate the input values
+                    
+                    if (milliseconds < 0 || seconds < 0 || minutes < 0)
+                    {
+                        MessageBox.Show("Time values cannot be negative.");
+                        return;
+                    }
+
+                    // Calculate the total time in milliseconds
+                    int totalTime = milliseconds + (seconds * 1000) + (minutes * 60000);
+
+                    // Determine the mouse button to be clicked
+                    string mouseButton = "";
+                    if (leftClick)
+                    {
+                        mouseButton = "left";
+                    }
+                    else if (middleClick)
+                    {
+                        mouseButton = "middle";
+                    }
+                    else if (rightClick)
+                    {
+                        mouseButton = "right";
+                    }
+
+                    // Start the auto clicker
+                    AutoClicker(totalTime, mouseButton);
+                }
+            }
+        }
+
+
+        ~Application()
+        {
+            // Ensure to dispose the timer when the form is closed
+            if (timer != null)
+            {
+                timer.Dispose();
+            }
+        }
+    }
+}
